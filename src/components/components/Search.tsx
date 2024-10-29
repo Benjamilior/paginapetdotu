6737f9b38e26496468f5ffe699a528ba15484ba6
benja <benjammunoz@alumnos.uai.cl>
2024-10-29 14:49:20 -0300

    cambios hechos en Hit para error

--- src/components/components/Search.tsx
+++ src/components/components/Search.tsx
@@ -2,7 +2,7 @@ import { liteClient as algoliasearch } from "algoliasearch/lite";
 import "instantsearch.css/themes/satellite.css";
 import { Hits, InstantSearch, SearchBox, Configure } from "react-instantsearch";
 
-import { Hit } from "./Hit";
+import { Hit } from "./hit";
 
 const searchClient = algoliasearch("LO5KTJKIRN", "b01fac50913e9c285746a8b5da1295cb");
 
