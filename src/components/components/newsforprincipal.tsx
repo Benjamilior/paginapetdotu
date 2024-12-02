d57ff5ce241401ec16df7c559c5bd857caf66c82
benja <benjammunoz@alumnos.uai.cl>
2024-12-02 10:53:42 -0300

    cambio de noticias

--- src/components/components/newsforprincipal.tsx
+++ src/components/components/newsforprincipal.tsx
@@ -12,11 +12,10 @@ export function WobbleCardDemo() {
       >
         <div className="max-w-xs">
           <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Gippity AI powers the entire universe
+            Comida natural para tu mascotas
           </h2>
           <p className="mt-4 text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+            Descubre una nueva tendencia que estan adoptando gran cantidad de tutores de mascotas.
           </p>
         </div>
         <Image
@@ -29,21 +28,19 @@ export function WobbleCardDemo() {
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 min-h-[300px]">
         <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
+          Las razas de perro mas inteligentes
         </h2>
         <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
+          ¿Cuales son las razas de perro mas inteligentes? ¿Esta tu mascota en la lista?
         </p>
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
         <div className="max-w-sm">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
+           Una solución integral para cuidar a tus mascotas
           </h2>
           <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+            Petvet, una innovadora solución para cuidar a tus mascotas. Encuentra en un solo lugar todo lo necesario para cuidar tus mascotas.
           </p>
         </div>
         <Image
